4e346709cef813a2c47e57156891352475aad9f1
NoKambing <NoKambing@gmail.com>
2023-09-26 14:18:04 +0700

    upload apk streaming 1

---
+++ application/views/login/template/footer.php
@@ -0,0 +1,8 @@
+<!-- jQuery -->
+<script src="http://localhost/streaming/assets/login/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="http://localhost/streaming/assets/login/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="http://localhost/streaming/assets/login/dist/js/adminlte.min.js"></script>
+</body>
+</html>
